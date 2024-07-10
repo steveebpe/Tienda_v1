034b9634c3ddd079fca9c3e1114b7498e8c65345
steveebpe <steveebp12@gmail.com>
2024-07-09 23:07:27 -0600

    Semana 6
    semana 6 update

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,29 @@
+package com.tienda.domain;
+
+import lombok.Data;
+import java.io.Serializable;
+import jakarta.persistence.*;
+
+@Data 
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable{
+ 
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column (name="id_categoria")        
+    
+  private Long idCategoria;
+  private String descripcion;
+  private String rutaImagen;
+  private boolean activo;
+  
+  public Categoria() { 
+  }
+  
+  public Categoria(String categoria, boolean activo) {
+      this.descripcion = categoria;
+      this.activo = activo;
+  }
+}
